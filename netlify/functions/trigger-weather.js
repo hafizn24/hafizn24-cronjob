f232a98a40a054a55b26518852759d9cf8348ab8
Hafiz <hafizn24@gmail.com>
2026-05-08 07:39:38 +0800

    housekeeping: remove code related to netlify

--- netlify/functions/trigger-weather.js
+++
@@ -1,6 +0,0 @@
-const { runWeatherCheck } = require('../lib/weather');
-
-module.exports.handler = async (event, context) => {
-  console.log('Weather triggered via HTTP at', new Date().toISOString());
-  return runWeatherCheck();
-};
--- netlify/functions/weather-cron.js
+++
@@ -1,7 +0,0 @@
-const { schedule } = require('@netlify/functions');
-const { runAIWeatherCheck } = require('../lib/ai-weather');
-
-module.exports.handler = schedule('*/30 * * * *', async (event, context) => {
-  console.log('Weather cron triggered at', new Date().toISOString());
-  return runAIWeatherCheck();
-});
